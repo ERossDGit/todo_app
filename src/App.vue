d1865ed4d9e9670fc618a83f486aaabed16f8237
ERossDGit <ross@simpleinsight.net>
2019-11-23 10:51:54 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a new Task">
+    <ul class="todos">
+      <li v-for="(todo, index) in todos" :key="todo.id">
+        <input class="completedCheck" type="checkbox" v-model="todo.completed">
+        <span class="todoLabel" :class="{completed: todo.completed}">
+          {{ todo.label }}
+        </span>  
+      </li>
+    </ul>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      currentTodo: ''
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+      this.currentTodo = '';
     }
   }
+};
+</script>
+
+<style>
+.completedCheck {
+  float: left;
 }
 </style>
